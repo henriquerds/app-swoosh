161d1669bd0135bb24e02e93e631fc6934fd84d2
Henrique Dos Santos <henrique@Henriques-MacBook-Pro.local>
2018-09-14 19:05:23 -0400

    learning to config buttons and use models

--- app-swoosh/app-swoosh/Controller/LeagueViewController.swift
+++ app-swoosh/app-swoosh/Controller/LeagueViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class LeagueViewController: UIViewController {
+    
+    var player: Player! //we make it an implicitly unwrapped beacause we don't want it to run unless there is a player type selected
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillViewControllerSegue", sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Henrique Dos Santos on 9/14/18.
+//  Copyright © 2018 HdosSant Co. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
